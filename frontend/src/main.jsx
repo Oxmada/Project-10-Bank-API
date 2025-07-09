83fbeae635c61a7f327946e0f790eb52ecb44d9d
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-07-09 19:09:18 +0300

    Ajout du store Redux et du slice d'authentification (authSlice)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router/router.jsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import './styles/main.css';
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
-)
+);
+
