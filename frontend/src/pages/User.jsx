8464d55573da06adb5b15cc4447d37685eb29153
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-07-13 19:26:16 +0300

    Ajout de la fonctionnalité déconnexion (logout)

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,11 +1,19 @@
-import { useSelector } from "react-redux"
-import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { logout } from "../redux/UserSlice"
 import Footer from "../components/Footer"
 import logo from "../assets/argentBankLogo.png"
 
 function User() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const firstName = useSelector((state) => state.user.firstName)
     const lastName = useSelector((state) => state.user.lastName)
+
+    const handleLogout = () => {
+        dispatch(logout())
+        navigate("/")
+    }
 
     return (
         <div>
@@ -19,10 +27,10 @@ function User() {
                         <i className="fa fa-user-circle"></i>
                         {firstName}
                     </Link>
-                    <Link className="main-nav-item" to="/">
+                    <button className="main-nav-item" onClick={handleLogout}>
                         <i className="fa fa-sign-out"></i>
                         Sign Out
-                    </Link>
+                    </button>
                 </div>
             </nav>
 
