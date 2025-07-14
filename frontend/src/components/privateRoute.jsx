cc682ef26a5dd2f91c71285365f88a8d1a0175bf
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-07-14 15:17:14 +0300

    Ajout de privateRoute.jsx pour sécuriser la route user

---
+++ frontend/src/components/privateRoute.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom"
+
+function PrivateRoute({ children }) {
+    const isLoggedIn = useSelector((state) => state.user.isLoggedIn)
+    return isLoggedIn ? children : <Navigate to="/sign-in" />
+
+}
+
+export default PrivateRoute
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,19 +1,28 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import SignIn from "../pages/SignIn";
-import User from "../pages/User";
+import { Routes, Route } from "react-router-dom"
+import Home from "../pages/Home"
+import SignIn from "../pages/SignIn"
+import User from "../pages/User"
+import PrivateRoute from "../components/privateRoute"
 
 function Router() {
     return (
         <Routes>
-            <Route path="/" element={< Home />} />
-            < Route path="/sign-in" element={< SignIn />} />
-            < Route path="/user" element={< User />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route
+                path="/user"
+                element={
+                    <PrivateRoute>
+                        <User />
+                    </PrivateRoute>
+                }
+            />
         </Routes>
-    );
+    )
 }
 
-export default Router;
+export default Router
+
 
 
 
